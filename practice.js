9118309d66c2494c052bec0f5a6b6730313287c0
rakeshboyiri <boyirirakesh@gmail.com>
2024-07-14 00:04:36 +0530

    files-added

--- practice.js
+++ practice.js
@@ -1,17 +1,7 @@
-var arr = [40,20,50,10,30]
-var sort = (arr,n) =>{
-    if (n==1){
-        return ;
-    }
-
-    for(var i = 0;i<n-2;i++){
-        if(arr[i]>arr[i+1]){
-            var temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-        }
-    }
-    sort(arr,n-1);
+const a = ()=>{
+    return "hello";
 }
-sort(arr,arr.length);
-console.log(arr);
+
+const b = () =>{
+    return "good moring";
+}
