c7ac74ba925ccc78bb236014aabe1b5e63754153
rakeshboyiri <boyirirakesh@gmail.com>
2024-07-13 11:45:13 +0530

    script.js code edited

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-
-const heading = React.createElement("h1",{id:"heading"},"Hello World from React!!!");
-const para = React.createElement("p",{id:"para"},heading);
-const parent = React.createElement("div",{id:"parent"},para);
-
-
-console.log(parent);
-
-
+const heading1 = React.createElement("h1",{id:"heading1"},"Im Heading 1");
+const heading2 = React.createElement("h2",{id:"heading2"},"Im Heading 2");
+const heading3 = React.createElement("h3",{id:"heading3"},"Im Heading 3");
+const para = React.createElement("p",{id:"para"},[heading1,heading2,heading3]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(para);
