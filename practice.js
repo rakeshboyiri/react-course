2c4f1b5e82163fe6420f433ada6854660216c86b
rakeshboyiri <boyirirakesh@gmail.com>
2024-07-14 00:07:37 +0530

    files-added

--- practice.js
+++ practice.js
@@ -1,14 +1,13 @@
-const arr = [40,30,50,70,10]
 
-for(var i = 0 ; i < arr.length ; i++){
-    for  (var j=0;j<arr.length-i-1;j++){
-        if(arr[j]>arr[j+1]){
-            var temp = arr[j]
-            arr[j] = arr[j+1]
-            arr[j+1] = temp
-        }
-    }
+/*
+
+we can call any function in js before declartion
+console.log(square(5));
+
+function square(n){
+    return n*n;
 }
-console.log(arr)
-y = 10,20;
-console.log(y);
+
+console.log(square(5));
+
+*/
