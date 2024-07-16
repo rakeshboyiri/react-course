1dd40088b5384e4c0571c7ad4463cfc8e54c4ee1
rakeshboyiri <boyirirakesh@gmail.com>
2024-07-16 10:05:23 +0530

    files-removed

--- script.js
+++
@@ -1,6 +0,0 @@
-const heading1 = React.createElement("h1",{id:"heading1"},"Im Heading 1");
-const heading2 = React.createElement("h2",{id:"heading2"},"Im Heading 2");
-const heading3 = React.createElement("h3",{id:"heading3"},"Im Heading 3");
-const para = React.createElement("p",{id:"para"},[heading1,heading2,heading3]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(para);
