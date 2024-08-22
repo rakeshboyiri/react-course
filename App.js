07d51069152a7c91a56cbb6cffcb6b014d16172c
rakeshboyiri <boyirirakesh.com>
2024-08-22 23:28:35 +0530

    files-edited

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = <h1 className="head">Namaste React</h1>
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(heading);
--- script.js
+++ script.js
@@ -7,4 +7,6 @@ const heading = React.createElement('div', {id:'parent'}, heading2);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading) 
+root.render(heading)
+
+// =>
